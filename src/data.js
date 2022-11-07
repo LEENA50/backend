7bbfa910078b59b696ade42801bbbcf765fbd97c
Leena Mahato <leenamahato5@gmail.com>
2022-11-07 20:11:23 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+var Users=[
+    {
+        id:1,
+        name:"abc",
+        email:"abc@gmail.com"
+    },
+    {
+        id:2,
+        name:"def",
+        email:"def@gmail.com"
+    },
+    {
+        id:3,
+        name:"ghi",
+        email:"ghi@gmail.com"
+    },
+    {
+        id:4,
+        name:"jkl",
+        email:"jkl@gmail.com"
+    }
+]
+
+
+module.exports=Users
